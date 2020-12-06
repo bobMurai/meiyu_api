26249c61b24b1b96668fd14c6e46f2895aa1d155
bobMurai <murai.sho@gmail.com>
2020-12-06 18:48:58 +0900

    user_controller及びregisterアクション作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
   mount_devise_token_auth_for 'User', at: 'auth'
-  # get 'tests/members_only', to: 'tests#members_only'
-  # resources :Users
+  post 'users/register', to: 'users#register'
   resources :chats
 end
