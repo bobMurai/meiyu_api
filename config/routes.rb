82dd88545b85e631895c0479bfa357de6e21baf8
bobMurai <murai.sho@gmail.com>
2020-12-10 00:43:29 +0900

    fr_syncアクションをsyncアクションに統合

--- config/routes.rb
+++ config/routes.rb
@@ -6,9 +6,7 @@ Rails.application.routes.draw do
   post 'friend_relations', to: 'friend_relations#create'
 
   get 'utils/sync', to: 'utils#sync'
-  get 'utils/fr_sync', to: 'utils#fr_sync'
 
-  # post 'users/register', to: 'users#register'
   mount_devise_token_auth_for 'User', at: 'auth'
 
 end
