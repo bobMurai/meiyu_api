9f04c711eae473dae0eee3c5dba19f92c42ad74b
bobMurai <murai.sho@gmail.com>
2020-12-09 17:44:42 +0900

    エラーハンドリング追加

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,12 @@
 class ApplicationController < ActionController::API
         include DeviseTokenAuth::Concerns::SetUserByToken
+        rescue_from Exception, with: :handle_err
+
+        private
+        # エラー時のレスポンスを返す
+        def handle_err(e)
+                header = {result: e.code, message: e.message}
+                res = {status: "fail", header: header}
+                render json: res
+        end
 end
