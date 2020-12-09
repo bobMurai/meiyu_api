2c3c0c86d17f3ca16bfc515f390c649a976e06aa
bobMurai <murai.sho@gmail.com>
2020-12-09 17:44:19 +0900

    APIのエンドポイント整備

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,21 @@
 Rails.application.routes.draw do
-  mount_devise_token_auth_for 'User', at: 'auth'
+  # resources :users do
+  #   resources :chats
+  #   resources :friend_relation_details
+  # end
+
+  # resources :friend_relations do
+  #   resources :chats
+  #   resources :friend_relation_details
+  # end
+
+  resources :users, only: [:update, :destroy]
+  resources :friend_relations, only: [:create, :update]
+
+  get 'utils/sync', to: 'utils#sync'
+  get 'utils/fr_sync', to: 'utils#fr_sync'
+
   post 'users/register', to: 'users#register'
-  resources :chats
+  mount_devise_token_auth_for 'User', at: 'auth'
+
 end
